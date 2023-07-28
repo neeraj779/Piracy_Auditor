eee1f381c254950b5f2f3bc9a8288e0d6f7394f8
Neeraj <neeraj79108@gmail.com>
2023-07-28 14:16:56 +0530

    created new view functions and templates

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.landingPage, name='landingPage'),
+    path('file/', views.file, name='file'),
+    path('text/', views.text, name='text'),
+    path('compare/', views.compare, name='compare'),
+    path('text-test-result/', views.result, name='result'),
+    path('file-test-result/', views.fileTestResult, name='fileTestResult'),
+    path('text-similarity-analysis/', views.textSimilarityAnalysis, name='textSimilarityAnalysis'),
+    path('file-similarity-analysis/', views.fileSimilarityAnalysis, name='twofilecompare1'),
+    
 ]
